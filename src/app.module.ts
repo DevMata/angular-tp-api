caaa8e25f81641b133fcd4ae73fdb775ce41f58d
Antonio Mata <hildeamp@gmail.com>
2021-03-04 13:04:58 -0600

    add: add Auth module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './type-orm-config.service';
 import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     UserModule,
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
     ProductModule,
+    AuthModule,
   ],
   providers: [TypeOrmConfigService],
 })
