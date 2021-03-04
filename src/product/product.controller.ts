79dbc26473c8f267cb355c8d97e9d52459dc46f3
Antonio Mata <hildeamp@gmail.com>
2021-03-03 23:46:06 -0600

    feat: add Product crud

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,24 @@
 import { Controller } from '@nestjs/common';
+import { Crud, CrudController } from '@nestjsx/crud';
+import { Product } from './entities/product.entity';
+import { ProductService } from './product.service';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller('product')
-export class ProductController {}
+@Crud({
+  model: { type: Product },
+  params: {
+    productId: {
+      primary: true,
+      field: 'productId',
+      type: 'number',
+    },
+  },
+  routes: {
+    exclude: ['createManyBase', 'replaceOneBase'],
+  },
+})
+@ApiTags('Products')
+@Controller('products')
+export class ProductController implements CrudController<Product> {
+  constructor(public service: ProductService) {}
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
+import { Product } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ProductService {}
+export class ProductService extends TypeOrmCrudService<Product> {
+  constructor(
+    @InjectRepository(Product) productRepository: Repository<Product>,
+  ) {
+    super(productRepository);
+  }
+}
