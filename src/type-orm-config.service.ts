e71312d7fb0de1c401cfa871b81b504160d92f37
Antonio Mata <hildeamp@gmail.com>
2021-03-04 15:29:23 -0600

    refactor: include SSL handling in TypeOrmConfig service

--- src/type-orm-config.service.ts
+++ src/type-orm-config.service.ts
@@ -6,14 +6,24 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     const {
       DATABASE_URL = 'postgres://myUser:myPassword@localhost:5432/angular_tp_db',
+      NODE_ENV,
     } = process.env;
+
+    const extra =
+      NODE_ENV === 'dev'
+        ? null
+        : {
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          };
 
     return {
       type: 'postgres',
       url: DATABASE_URL,
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
-      ssl: true,
+      extra,
     };
   }
 }
