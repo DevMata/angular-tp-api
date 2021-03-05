a82c1f1850117bba4f75baadb5fdb0fa75aad118
Antonio Mata <hildeamp@gmail.com>
2021-03-04 19:34:15 -0600

    feat: add Login DTO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { LoginDto } from './login.dto';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   @UseGuards(AuthGuard('local'))
+  @ApiBody({ type: LoginDto })
   @Post('login')
   async login(@Request() req) {
     return req.user;
