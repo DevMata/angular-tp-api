33dcf864a039623049be3868041d068ae528d2f3
Antonio Mata <hildeamp@gmail.com>
2021-03-10 22:43:27 -0600

    feat: define Product entity

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,15 @@
-export class Product {}
+import { Column, CreateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'numeric', precision: 12, scale: 2 })
+  price: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
