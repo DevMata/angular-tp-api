c60428741aa8a8591587f168910f070a42954b3f
Antonio Mata <hildeamp@gmail.com>
2021-03-10 22:38:50 -0600

    feat: add product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './type-orm-config.service';
 import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
     AuthModule,
     UserModule,
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    ProductModule,
   ],
   providers: [TypeOrmConfigService],
 })
