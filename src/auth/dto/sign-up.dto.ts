b87854968d83bb413f537545534946d76a8d3fd9
Antonio Mata <hildeamp@gmail.com>
2021-03-10 21:28:19 -0600

    feat: add SignUp DTO

---
+++ src/auth/dto/sign-up.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class SignUpDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  password: string;
+
+  @IsString()
+  @MinLength(8)
+  passwordConfirmation: string;
+}
